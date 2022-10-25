ba1bbc09fbf621a5a46dd5835af86d2ad91e35f2
Ryan Armstrong <armstrongryan11@gmail.com>
2022-10-24 19:12:33 -0600

    added panic recovery code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"AWD_FinalProject.ryanarmstrong.net/internal/data"
+	"AWD_FinalProject.ryanarmstrong.net/internal/jsonlog"
 	_ "github.com/lib/pq"
 )
 
@@ -34,7 +35,7 @@ type config struct {
 // Dependency Injection
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -49,16 +50,16 @@ func main() {
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle connections time")
 	flag.Parse()
 	// Create a logger
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 	// Create the connection pool
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
 	// Log the successful connection pool
-	logger.Println("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 	// Create an instance of our application struct
 	app := &application{
 		config: cfg,
@@ -72,14 +73,18 @@ func main() {
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		ErrorLog:     log.New(logger, "", 0),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 	// Start our server
-	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("Starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 // The openDB() function returns a *sql.DB connection pool
