76de7ec9c5f3e67d7decc72bf31f32fcb1bdf872
Ryan Armstrong <armstrongryan11@gmail.com>
2022-10-29 11:18:56 -0600

    Users can be added

---
+++ cmd/api/users.go
@@ -0,0 +1,65 @@
+// Filename: cmd/api/users.go
+
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"AWD_FinalProject.ryanarmstrong.net/internal/data"
+	"AWD_FinalProject.ryanarmstrong.net/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Hold data from the request body
+	var input struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Parse the request body into the anonymous struct
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the data to a new struct
+	user := &data.User{
+		Username:  input.Username,
+		Email:     input.Email,
+		Activated: false,
+	}
+	// Generate a password hash
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// Perform validation
+	v := validator.New()
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Insert the data in the database
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("username", "a user with this username already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Write a 201 Created Status
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
